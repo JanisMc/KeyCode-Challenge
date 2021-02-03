ea93b59bea18244834c08c1e7e610fbb7ea4b730
Janis <janismcluckie@googlemail.com>
2021-02-03 18:32:44 +0000

    added app.js to hide info/display welcome

---
+++ app.js
@@ -0,0 +1,14 @@
+const info = document.getElementById("info");
+const welcome = document.getElementById("welcome");
+
+
+// hide info screen 
+info.style.display = "none";
+
+document.addEventListener("keypress", (event) => {
+    console.log(event);
+    // if welcome visible -
+    // get rid of welcome screen 
+    // replace with info screen to display - event key / event code / event which 
+    
+})
